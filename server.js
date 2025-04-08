2de54a29fcf26eb9c609aa742fc64ed0a3a904ca
Ryan <ryan@example.com>
2025-04-08 19:28:13 -0400

    Fix PORT for Render

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('Hello from the Vendorai API!');
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
